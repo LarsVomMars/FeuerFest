96284338296d07a1475d977aa0000e97d0ea2fa6
LarsVomMars <developer@kroenner.eu>
2023-10-04 23:05:56 +0200

    Add basic table cells

---
+++ src/lib/components/Table/Cells/Text/EditableTextCell.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import TextCell from "./TextCell.svelte";
+
+    export let value: string;
+    export let onChange: (value: string) => void;
+
+    let edit = false;
+    let input: HTMLInputElement;
+
+    let prevValue = value;
+
+    const click = () => {
+        if (edit) return;
+        edit = true;
+        setTimeout(() => input.focus(), 0);
+    };
+
+    const blur = () => {
+        edit = false;
+        if (prevValue !== value) {
+            onChange(value);
+        }
+    };
+
+    const keypress = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            if (edit) blur();
+            else click();
+        }
+    };
+</script>
+
+<div
+    class="w-full h-full"
+    on:click={click}
+    on:keypress={keypress}
+    role="textbox"
+    tabindex="0"
+>
+    {#if edit}
+        <div class="w-full h-full">
+            <input bind:value bind:this={input} class="w-full" on:blur={blur} />
+        </div>
+    {:else}
+        <TextCell {value} />
+    {/if}
+</div>
---
+++ src/lib/components/Table/Cells/Text/TextCell.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let value: string;
+</script>
+
+<div class="w-full h-full">
+    {#if !value}
+        &nbsp;
+    {:else}
+        {value}
+    {/if}
+</div>
