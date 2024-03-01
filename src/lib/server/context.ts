5c7dc24448202e41c95cd0b5d0faaef57b2c4f0a
LarsVomMars <developer@kroenner.eu>
2024-03-01 23:50:09 +0100

    init

---
+++ src/lib/server/context.ts
@@ -0,0 +1,8 @@
+import type { RequestEvent } from "@sveltejs/kit";
+import type { inferAsyncReturnType } from "@trpc/server";
+
+export async function createContext(_event: RequestEvent) {
+    return {};
+}
+
+export type Context = inferAsyncReturnType<typeof createContext>;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+    <h1>FeuerFest</h1>
+</main>
